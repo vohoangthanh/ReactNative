c84d2618f15fd489448fc13604a8b5ab82f48621
Công Tuyền ✓ <94374796+ctuyendeveloper@users.noreply.github.com>
2023-07-19 17:26:54 +0700

    testfilepush

---
+++ src/testfilepush.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const testfilepush = () => {
+  return (
+    <View>
+      <Text>testfilepush</Text>
+    </View>
+  )
+}
+
+export default testfilepush
+
+const styles = StyleSheet.create({})
