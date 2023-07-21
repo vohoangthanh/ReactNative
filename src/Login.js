8d39a4de712b838d8d9a5f4d2e7d25c36595f737
vohoangthanh <thanhnho270701@gmail.com>
2023-07-21 12:16:28 +0700

    thanhho

--- src/Login.js
+++ src/Login.js
@@ -14,13 +14,9 @@ const Login = (props) => {
   };
   return (
     <View style={styles.body}>
-      <View style={styles.top}></View>
-      <View style={styles.bottom}>
-        <View style={styles.bottomview2}>
-          <Image style={styles.anhmonan} source={require('../media/anh.png')} />
-        </View>
-        <Text style={styles.text}>MyFPL</Text>
-        <View style={styles.textinputuser}>
+      <Text>MyFPL</Text>
+      
+        {/* <View style={styles.textinputuser}>
           <Image style={styles.anh2} source={require('../media/user.png')} />
           <TextInput value={username} onChangeText={setUsername} style={styles.textinput} placeholder='Username'></TextInput>
         </View>
@@ -30,9 +26,7 @@ const Login = (props) => {
         </View>
         <Pressable style={styles.button} onPress={handleLogin}>
         <Text style={styles.text2}>Login</Text>
-        </Pressable>
-        <Text style={styles.text3}>Forgot your password?</Text>
-      </View>
+        </Pressable> */}
     </View>
   )
 }
@@ -46,79 +40,5 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
   },
-  bottom: {
-    borderRadius: 450,
-    width: '200%',
-    height: '200%',
-    alignItems: 'center',
-    backgroundColor: '#feb02a'
-  },
-  top: {
-    width: '100%',
-    height: '26%',
-  },
-  anhmonan: {
-    top: '5%',
-    borderRadius: 50,
-    width: '85%',
-    height: '85%',
-  },
-  bottomview2: {
-    alignItems: "center",
-    backgroundColor: '#AAEEFF',
-    borderRadius: 50,
-    width: '20%',
-    height: '11%',
-    top: '-5.5%'
-  },
-  text: {
-    color: '#7C85FF',
-    fontFamily: 'Yu Gothic UI',
-    fontStyle: 'normal',
-    fontSize: 36,
-    top: '-2%',
-  },
-  textinputuser: {
-    backgroundColor: 'white',
-    borderRadius: 16,
-    width: '41%',
-    height: '3.5%',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  anh2: {
-    marginLeft: '2%',
-    marginRight: '7%',
-  },
-  textinputuser2: {
-    backgroundColor: 'white',
-    borderRadius: 16,
-    width: '41%',
-    height: '3.5%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: '5%',
-    marginBottom: '5%',
-  },
-  button: {
-    alignItems: 'center',
-    width: '13%',
-    height: '4.5%',
-    borderRadius: 25,
-    backgroundColor: '#fc8a03',
-    marginBottom: '2.5%',
-  },
-  text2: {
-    color: '#FFF',
-    fontFamily: 'Yu Gothic UI',
-    fontStyle: 'normal',
-    lineHeight: 24,
-    fontSize: 24,
-    top: '35%',
-  },
-  text3: {
-    color: '#FFF',
-    fontStyle: 'normal',
-    fontSize: 18,
-  },
+  
 })
